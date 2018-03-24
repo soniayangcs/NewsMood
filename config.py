65f6a442a465b7cf8980164ffbd93659f4f0ccaa
Sonia Yang <soniayangcs@gmail.com>
2018-03-24 04:06:56 -0700

    update

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = 'GTLuC0phSyzs1VpDtnAgvqxk1'
+consumer_secret = 'ZKJxf18tvU6BFvwhsDTh5omW05jD0QAq46dGYwSV6hzF9zSfCi'
+access_token = '2668526293-pQPVZCM9PPi9RDZ2ApsBZgqECjocgqONXntpKYe'
+access_token_secret = 'mXAJfyhWThpfyVg0tOByaYoCdqNnmKedkmHZx7bQmuKSg'
